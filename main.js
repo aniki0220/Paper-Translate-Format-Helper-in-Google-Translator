efbfb31a3d6ac0f9d9dbb88ee7d0249589ff9bea
aniki0220 <spade5369@hotmail.com.tw>
2018-10-19 14:18:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         GoogleTranslate_paperHelper
-// @version      1.0
+// @version      1.3.3
 // @description  ez way to translate with google
 // @author       NDM
-// @include      https://translate.google.com/*
+// @include      https://translate.google.com*
 
 // @grant        unsafeWindow
 // ==/UserScript==
@@ -17,7 +17,10 @@
             newButton1.style.cursor="pointer";
             newButton1.onclick = ()=>{
                 source.value = source.value.replace(/\n/g, " ");
-                source.value = source.value.replace(/\./g, ".\n\n");
+                source.value = source.value.replace(/\. /g, ".\n\n");
+                source.value += "\n\n====\n"
+                for(i= source.textLength+1;i<=1001;i++)
+                    source.value += "💩";
             }
             var newButton2 = document.createElement("span");
             newButton2.innerHTML="比對";
@@ -25,7 +28,7 @@
             newButton2.style.cursor="pointer";
             newButton2.onclick = ()=>{
                 for(var i = 0; i< result_box.children.length; i++)
-                result_box.children[i].innerHTML = result_box.children[i].title+"<br>" + result_box.children[i].innerHTML 
+                result_box.children[i].innerHTML = result_box.children[i].title+"<br><font color=blue>" + result_box.children[i].innerHTML +"</font>"
             }
             buttonSite.appendChild(newButton1);
             buttonSite.appendChild(newButton2);
