04eff106ede7d7cdb26e2455283cae62e08e45a5
Wei <BCS106119@gm.ntcu.edu.tw>
2019-10-03 15:51:02 +0800

    20191003 hotfix
    ERROR: Execution of script 'GoogleTranslate_paperHelper' failed! newButton3 is not defined

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GoogleTranslate_paperHelper
-// @version      2.2.0
+// @version      2.2.1
 // @description  ez way to C and V
 // @author       NDM
 // @include      https://translate.google.com*
@@ -14,7 +14,7 @@
 
     var buttonSite = document.getElementsByClassName("tlid-input-button-container")[0]
     var newButton1 = document.createElement("span");
-    var newButton2 = document.createElement("span");
+    var newButton3 = document.createElement("span");
     var s = ""
     newButton1.innerHTML="排版";
     newButton1.style.borderStyle="solid";
@@ -42,36 +42,36 @@
     }
 
 
-    newButton2.innerHTML="比對";
-    newButton2.style.borderStyle="solid";
-    newButton2.style.cursor="pointer";
-    newButton2.onclick = ()=>{
-        var targetSpans = document.getElementsByClassName("tlid-translation")[0].getElementsByTagName("span");
-        // console.log(targetSpans);
+//     newButton2.innerHTML="比對";
+//     newButton2.style.borderStyle="solid";
+//     newButton2.style.cursor="pointer";
+//     newButton2.onclick = ()=>{
+//         var targetSpans = document.getElementsByClassName("tlid-translation")[0].getElementsByTagName("span");
+//         // console.log(targetSpans);
         
-        if (s == ""){
-            alert("請先按下「排版」，才可進行「比對」")
-            return false
-        }
-        if (targetSpans.length == 0){
-            alert("字數過多，1000字以內才可使用「比對」功能")
-            return false
-        }
+//         if (s == ""){
+//             alert("請先按下「排版」，才可進行「比對」")
+//             return false
+//         }
+//         if (targetSpans.length == 0){
+//             alert("字數過多，1000字以內才可使用「比對」功能")
+//             return false
+//         }
         
-        var separators = ['.\n\n', '\;'];
-        s = source.value.split(new RegExp(separators.join('|'),'g'));
+//         var separators = ['.\n\n', '\;'];
+//         s = source.value.split(new RegExp(separators.join('|'),'g'));
 
-        for(i in targetSpans){
-            // console.log(s[i][0]);
-            while(s[i][0] == " ")
-                s[i] = s[i].substr(1)
-            // targetSpans[i].innerHTML += "\n<br><font color=blue>" + s[i] + ".</font>";
-            var temp = targetSpans[i].innerHTML; 
-            targetSpans[i].innerHTML = "<font color=blue>" + s[i] + ".</font><br>" + temp; 
-        }
-        console.log(s);
+//         for(i in targetSpans){
+//             // console.log(s[i][0]);
+//             while(s[i][0] == " ")
+//                 s[i] = s[i].substr(1)
+//             // targetSpans[i].innerHTML += "\n<br><font color=blue>" + s[i] + ".</font>";
+//             var temp = targetSpans[i].innerHTML; 
+//             targetSpans[i].innerHTML = "<font color=blue>" + s[i] + ".</font><br>" + temp; 
+//         }
+//         console.log(s);
         
-    }
+//     }
 
     newButton3.innerHTML="比對";
     newButton3.style.borderStyle="solid";
